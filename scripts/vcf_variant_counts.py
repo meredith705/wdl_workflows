521bad2d7432f1928390f61340ddc0f10b6e4335
Melissa <memeredith@csumb.edu>
2025-12-05 14:44:37 -0800

    add variant type/length plot

--- scripts/vcf_variant_counts.py
+++ scripts/vcf_variant_counts.py
@@ -94,6 +94,8 @@ def plot_violin_perSample(vcf_data, vcf_prefix):
 	""" set up the figure to plot a violin """
 	fig, axs = plt.subplots(figsize=(8,4))
 
+	print(vdf_data)
+
 	vcf_df = pd.DataFrame(vcf_data)
 	print('type', type(vcf_df))
 	print('len', len(vcf_df))
