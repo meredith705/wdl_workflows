305c939a1007c7cb8143f05bedf153646a5e3b98
Melissa <memeredith@csumb.edu>
2026-08-13 13:45:02 -0700

    add hifiasm slurm

--- sbatch_scripts/hifiasm.sh
+++ sbatch_scripts/hifiasm.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #SBATCH --job-name=hifiasm
 #SBATCH --mail-type=FAIL,END
-#SBATCH --partition=long
-#SBATCH --mail-user=mmmeredi@ucsc.edu
+#SBATCH --partition=largemem
+#SBATCH --mail-user=melissa@datatecnica.com
 #SBATCH --nodes=1
 #SBATCH --mem=1000G
 #SBATCH --cpus-per-task=96
 #SBATCH --output=%x.%j.log
 #SBATCH --time=12:00:00
 
-# Usage: sbatch script.sh sample_name file1.bam file2.bam file3.bam
+# Usage: sbatch --mail-type=BEGIN,TIME_LIMIT_90,END script.sh sample_name file1.bam file2.bam file3.bam
 
 set -eox
 
@@ -19,6 +19,8 @@ date
 hostname
 pwd
 
+module load hifiasm
+module load samtools 
 
 sample_name=$1
 shift
@@ -30,12 +32,8 @@ done
 
 
 # run hifiasm
+hifiasm -t $SLURM_CPUS_PER_TASK -o $sample_name.hifiasm.ont ${sample_name}.merged.fastq 2> $sample_name.hifiasm.log
 
-srun docker run --rm -u $(id -u):$(id -g) -v $(pwd):/workspace -w /workspace meredith705/hifiasm@sha256:c86d7f2750a52a75814828462fe4d6dad5d3980c95ddadb6ed62af629a17aad7 hifiasm \
-                                                                                                                                        -t 96 \
-                                                                                                                                        --ont \
-                                                                                                                                        -o $sample_name.hifiasm.ont \
-                                                                                                                                        ${sample_name}.merged.fastq 2> $sample_name.hifiasm.log
 
 awk '/^S/{print ">"$2;print $3}' $sample_name.hifiasm.ont.bp.hap1.p_ctg.gfa > $sample_name.hifiasm.ont.bp.hap1.p_ctg.fa
 awk '/^S/{print ">"$2;print $3}' $sample_name.hifiasm.ont.bp.hap2.p_ctg.gfa > $sample_name.hifiasm.ont.bp.hap2.p_ctg.fa
@@ -44,3 +42,4 @@ bgzip -@ 64 $sample_name.hifiasm.ont.bp.hap1.p_ctg.fa
 bgzip -@ 64 $sample_name.hifiasm.ont.bp.hap2.p_ctg.fa
 
 bgzip -@ 96 ${sample_name}.merged.fastq
+
