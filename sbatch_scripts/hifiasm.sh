ef5f4a6451b9acf55535d68c31bd7a8c7105fd71
Melissa <memeredith@csumb.edu>
2026-08-13 12:48:43 -0700

    add hifiasm batch docker

---
+++ sbatch_scripts/hifiasm.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#SBATCH --job-name=hifiasm
+#SBATCH --mail-type=FAIL,END
+#SBATCH --partition=long
+#SBATCH --mail-user=mmmeredi@ucsc.edu
+#SBATCH --nodes=1
+#SBATCH --mem=1000G
+#SBATCH --cpus-per-task=96
+#SBATCH --output=%x.%j.log
+#SBATCH --time=12:00:00
+
+# Usage: sbatch script.sh sample_name file1.bam file2.bam file3.bam
+
+set -eox
+
+# go to directory 
+cd ...
+date
+hostname
+pwd
+
+
+sample_name=$1
+shift
+
+for READS in "$@"; do
+    samtools fastq -@ 96 $READS >> ${sample_name}.merged.fastq
+done
+
+
+
+# run hifiasm
+
+srun docker run --rm -u $(id -u):$(id -g) -v $(pwd):/workspace -w /workspace meredith705/hifiasm@sha256:c86d7f2750a52a75814828462fe4d6dad5d3980c95ddadb6ed62af629a17aad7 hifiasm \
+                                                                                                                                        -t 96 \
+                                                                                                                                        --ont \
+                                                                                                                                        -o $sample_name.hifiasm.ont \
+                                                                                                                                        ${sample_name}.merged.fastq 2> $sample_name.hifiasm.log
+
+awk '/^S/{print ">"$2;print $3}' $sample_name.hifiasm.ont.bp.hap1.p_ctg.gfa > $sample_name.hifiasm.ont.bp.hap1.p_ctg.fa
+awk '/^S/{print ">"$2;print $3}' $sample_name.hifiasm.ont.bp.hap2.p_ctg.gfa > $sample_name.hifiasm.ont.bp.hap2.p_ctg.fa
+
+bgzip -@ 64 $sample_name.hifiasm.ont.bp.hap1.p_ctg.fa
+bgzip -@ 64 $sample_name.hifiasm.ont.bp.hap2.p_ctg.fa
+
+bgzip -@ 96 ${sample_name}.merged.fastq
